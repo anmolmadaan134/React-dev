b428c05d8f11fe1a5b3299ec0fbcbc498ed2dc28
anmolmadaan368 <[anmolmad503@gmail.com]>
2024-05-30 12:39:31 +0530

    Counter react project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter,setCounter] = useState(10)
+
+  //let counter = 3;
+
+  const addValue = ()=> {
+    // console.log("value added",Math.random());
+    // counter = counter+1
+    if(counter<20){
+    setCounter(counter + 1);}
+    
+  }
+
+  const removeValue= ()=>{
+    if(counter>0){
+    setCounter(counter-1);}
+  }
+
+  return (
+    <>
+    <h1>React developer</h1>    
+    <h2>Counter Value: {counter}</h2>
+
+    <button
+    onClick={addValue}>Add Value {counter}</button>
+    <br/>
+    <button onClick={removeValue}>Remove Value {counter}</button>
+        
+    </>
+  )
+}
+
+export default App
